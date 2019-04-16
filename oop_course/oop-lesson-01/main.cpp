702b8b790eb8ff0891e6cd12107af9c16b9a539d
Arceniy Sysoev <46384242+perceniy@users.noreply.github.com>
2019-04-16 14:05:45 +0300

    Create class power

--- oop_course/oop-lesson-01/main.cpp
+++ oop_course/oop-lesson-01/main.cpp
@@ -1,6 +1,50 @@
 #include <iostream>
+#include <math.h>
+
+using namespace std;
+
+// Task 1. Create class Power.
+
+class Power {
+private:
+    double var;
+    double power;
+public:
+    Power() {
+        var = 0;
+        power = 1;
+    }
+    Power(double var, double power) {
+
+        if ((power == 0.0 ) & ( var == 0.0)) {
+            cout << "Unexpected input, set power to 1" << endl;
+            this->var = 0;
+            this->power = 1;
+        } else {
+        this->var = var;
+        this->power = power;
+        }
+    }
+    double set_params(double var, double power) {
+        if ((power == 0.0) & (var == 0.0)) {
+            cout << "Unexpected input, set power to 1" << endl;
+            this->var = 0;
+            this->power = 1;
+        } else {
+            this->var = var;
+            this->power = power;
+            cout << "Parameters was set successfully" << endl;
+        }
+    }
+    double calculate() {
+        return pow(var, power);
+    }
+};
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    Power myPower(0, 0);
+    cout << myPower.calculate() << endl;
+    myPower.set_params(2, 3);
+    cout << myPower.calculate() << endl;
+    return EXIT_SUCCESS;
 }
