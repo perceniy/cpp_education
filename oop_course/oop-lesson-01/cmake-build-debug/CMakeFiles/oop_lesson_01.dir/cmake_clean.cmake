adda88e1c354fcb1a62e9ea493a05c63209ac36c
sysoevuchi <sysoev@uchi.ru>
2019-04-16 12:34:45 +0300

    oop-lesson-01 blank project

---
+++ oop_course/oop-lesson-01/cmake-build-debug/CMakeFiles/oop_lesson_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_lesson_01.dir/main.cpp.o"
+  "oop_lesson_01.pdb"
+  "oop_lesson_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_lesson_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
