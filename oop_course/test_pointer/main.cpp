4eef1c546d8a77659f8322215b2734121478008b
perceniy <perceniy@gmail.com>
2019-04-22 17:16:17 +0300

    stage test_pointer

--- oop_course/test_pointer/main.cpp
+++ oop_course/test_pointer/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 
+//This is pointers and links magic
+
 class Date
 {
 private:
@@ -8,7 +10,8 @@ private:
     int m_year;
 
 public:
-    Date* date_next;
+    Date* datePointer;
+    Date** pointerPointer;
 
     Date(int day, int month, int year)
     {
@@ -25,6 +28,12 @@ public:
     int getDay() const { return m_day; }
     int getMonth() const { return m_month; }
     int getYear() const { return m_year; }
+
+    void printPointerAddress(){
+        /*Мы печатаем адрес переменной-указателя на класс*/
+        pointerPointer = &datePointer;
+        std::cout << pointerPointer << '\n';
+    };
 };
 
 // Мы передаем объект date по константной ссылке, чтобы избежать создания копии объекта date
@@ -33,30 +42,69 @@ void printDate(const Date &date)
     std::cout << date.getDay() << "." << date.getMonth() << "." << date.getYear() << '\n';
 }
 
-void testFunc(int* a) {
-    std::cout << a << std::endl;
-    std::cout << *a << std::endl;
-
+void printDatePointer(Date* date) {
+    (*date).setDate(20,3,2019);
+    std::cout << (*date).getDay() << "." << (*date).getMonth() << "." << (*date).getYear() << '\n';
+    std::cout << date << '\n';
 }
+
+struct type {
+    int i;
+
+    type(): i(3) {}
+
+    void f(int v) const {
+        // this->i = v;                 // compile error: this is a pointer to const
+        const_cast<type*>(this)->i = v; // OK as long as the type object isn't const
+    }
+};
 
 int main()
 {
     Date date(12, 11, 2018);
     printDate(date);
 
-    Date* dateTest;
+    Date* pointerTest;
 
-    dateTest = &date;
+    pointerTest = &date;
 
-    date.date_next = dateTest;
+    date.datePointer = pointerTest;
 
-    std::cout << date.date_next << std::endl;
+    std::cout << date.datePointer << std::endl;
     std::cout << &date << std::endl;
 
+    printDatePointer(date.datePointer);
 
-    int b = 6;
+    date.printPointerAddress();
 
-    testFunc(&b);
 
+
+    int i{3};                 // i is not declared const
+    const int& rci = i;
+    const_cast<int&>(rci) = 4; // OK: modifies i
+    std::cout << "i = " << i << '\n';
+
+    uint16_t q = 3;
+
+    std::cout << "q = " << q << "; size of q: " << sizeof(q)<<'\n';
+
+    type t2;
+
+    std::cout << "t2.i = " << t2.i << '\n';
+
+    type t; // if this was const type t, then t.f(4) would be undefined behavior
+    t.f(4);
+    std::cout << "type::i = " << t.i << '\n';
+
+    const int j = 3; // j is declared const
+    int* pj = const_cast<int*>(&j);
+    // *pj = 4;      // undefined behavior
+
+    void (type::* pmf)(int) const = &type::f; // pointer to member function
+    // const_cast<void(type::*)(int)>(pmf);   // compile error: const_cast does
+    // not work on function pointers
+
+    char a = 'a';
+    std::cout << sizeof(a) << std::endl;
     return 0;
 }
