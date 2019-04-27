9c43c451c2b2340fd5e4a4b3880cbf6201b7ba86
Arsceniy Sysoev <asysoev.msk@gmail.com>
2019-04-27 12:36:46 +0300

    task 3b

---
+++ oop_course/task3b/cmake-build-debug/CMakeFiles/task3b.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/task3b.dir/main.cpp.o"
+  "task3b.pdb"
+  "task3b"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/task3b.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
