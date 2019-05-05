cfc8266714a0264e94ddba9127bbe1faa70b1612
Arsceniy Sysoev <asysoev.msk@gmail.com>
2019-05-05 20:00:05 +0200

    oop lesson 05

---
+++ oop_course/oop-lesson-05/cmake-build-debug/CMakeFiles/oop_lesson_05.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_lesson_05.dir/main.cpp.o"
+  "oop_lesson_05.pdb"
+  "oop_lesson_05"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_lesson_05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
