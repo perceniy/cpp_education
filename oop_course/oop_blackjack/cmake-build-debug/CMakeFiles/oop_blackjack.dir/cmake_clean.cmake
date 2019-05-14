a32b3e80a03b8a1b3d0d6099f4206ef84bd8fe78
Arsceniy Sysoev <asysoev.msk@gmail.com>
2019-05-14 15:30:26 +0200

    Blackjack

---
+++ oop_course/oop_blackjack/cmake-build-debug/CMakeFiles/oop_blackjack.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop_blackjack.dir/main.cpp.o"
+  "oop_blackjack.pdb"
+  "oop_blackjack"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop_blackjack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
