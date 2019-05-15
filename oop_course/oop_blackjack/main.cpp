0afff9fc771fdcb01349e33dfb5e4dd1905e07fb
Arsceniy Sysoev <asysoev.msk@gmail.com>
2019-05-15 12:25:46 +0300

    oop blackjack

--- oop_course/oop_blackjack/main.cpp
+++ oop_course/oop_blackjack/main.cpp
@@ -1,100 +1,80 @@
-#include <vector>
 #include <iostream>
+#include <string>
+#include <vector>
 #include <algorithm>
-#include <random>
+#include <ctime>
 
 using namespace std;
 
-class Card {
+class Card
+{
 public:
-
     enum rank {
-        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING
+        ACE = 1, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+        JACK, QUEEN, KING
     };
     enum suit { CLUBS, DIAMONDS, HEARTS, SPADES };
 
-    Card(rank r, suit s, bool ifu) : m_Rank(r), m_Suit(s), m_IsFaceUp(ifu) {}
-    Card(rank r, suit s) : m_Rank(r), m_Suit(s), m_IsFaceUp(false) {}
-
-    int GetValue() const {
-        //если карта перевернута лицом вниз, ее значение равно О
-        int value = 0;
-        if (m_IsFaceUp)
-        {
-            // значение - это число, указанное на карте
-            value = m_Rank;
-            // значение равно 10 для JACK, QUEEN и KING
-            if (value > 10)
-            {
-                value = 10;
-            }
-        }
-        return value;
-    }
-
-    void Flip() {
-        m_IsFaceUp = !(m_IsFaceUp);
-    }
-
+    // перегружаем оператор<<, чтобы можно было отправить объект
+    // типа Card в стандартный поток вывода
     friend ostream& operator<<(ostream& os, const Card& aCard);
+
+    Card(rank r = ACE, suit s = SPADES, bool ifu = true);
+
+    // возвращает значение карты от 1 до 11
+    int GetValue() const;
+
+    //переворачивает карту: карта, лежащая лицом вверх,
+    // переворачивается лицом вниз и наоборот
+    void Flip();
+
 private:
     rank m_Rank;
     suit m_Suit;
     bool m_IsFaceUp;
 };
 
+Card::Card(rank r, suit s, bool ifu) : m_Rank(r), m_Suit(s), m_IsFaceUp(ifu)
+{}
 
-// перегружает оператор <<, чтобы получить возможность отправить
-// объект типа Card в поток cout
-ostream& operator<<(ostream& os, const Card& aCard)
+int Card::GetValue() const
 {
-    const string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9","10", "J", "Q", "K" };
-    const string SUITS[] = { "c", "d", "h", "s" };
-
-    if (aCard.m_IsFaceUp)
+    //если карта перевернута лицом вниз ее значение равно О
+    int value = 0;
+    if (m_IsFaceUp)
     {
-        os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
+        // значение - это число, указанное на карте
+        value = m_Rank;
+        // значение равно 10 для JACK, QUEEN и KING
+        if (value > 10)
+        {
+            value = 10;
+        }
     }
-    else
-    {
-        os << "XX";
-    }
-
-    return os;
+    return value;
 }
 
+void Card::Flip()
+{
+    m_IsFaceUp = !(m_IsFaceUp);
+}
 
 class Hand
 {
 public:
     Hand();
     // виртуальный деструктор
-    virtual ~Hand() {
-        Clear();
-    };
+    virtual ~Hand();
 
     // добавляет карту в руку
     void Add(Card* pCard);
 
     // очищает руку от карт
-    void Clear()
-    {
-        // проходит по вектору, освобождая всю память в куче
-        vector<Card*>::iterator iter = m_Cards.begin();
-
-        for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
-        {
-            delete *iter;
-            *iter = 0;
-        }
-        // очищает вектор указателей
-        m_Cards.clear();
-    }
+    void Clear();
 
     //получает сумму очков карт в руке, присваивая тузу
     // значение 1 или 11 в зависимости от ситуации
     int GetTotal() const;
-
 
 protected:
     vector<Card*> m_Cards;
@@ -106,36 +86,58 @@ Hand::Hand()
 }
 // деструктор по-прежнему виртуальный
 // это уже можно не обозначать
+Hand::~Hand()
+{
+    Clear();
+}
 
 void Hand::Add(Card* pCard)
 {
     m_Cards.push_back(pCard);
 }
 
+void Hand::Clear()
+{
+    // проходит по вектору, освобождая всю память в куче
+    vector<Card*>::iterator iter = m_Cards.begin();
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+    {
+        delete *iter;
+        *iter = 0;
+    }
+    // очищает вектор указателей
+    m_Cards.clear();
+}
+
 int Hand::GetTotal() const
 {
     // если карт в руке нет, возвращает значение 0
-    if (m_Cards.empty()) {
+    if (m_Cards.empty())
+    {
         return 0;
     }
 
     //если первая карта имеет значение 0, то она лежит рубашкой вверх:
     // вернуть значение 0
-    if (m_Cards[0]->GetValue() == 0) {
+    if (m_Cards[0]->GetValue() == 0)
+    {
         return 0;
     }
 
     // находит сумму очков всех карт, каждый туз дает 1 очко
     int total = 0;
     vector<Card*>::const_iterator iter;
-    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter) {
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+    {
         total += (*iter)->GetValue();
     }
 
     // определяет, держит ли рука туз
     bool containsAce = false;
-    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter) {
-        if ((*iter)->GetValue() == Card::ACE) {
+    for (iter = m_Cards.begin(); iter != m_Cards.end(); ++iter)
+    {
+        if ((*iter)->GetValue() == Card::ACE)
+        {
             containsAce = true;
         }
     }
@@ -151,78 +153,58 @@ int Hand::GetTotal() const
     return total;
 }
 
+// абстрактный класс
 class GenericPlayer : public Hand
 {
+    friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
+
 public:
-    GenericPlayer() = default;
+    GenericPlayer(const string& name = "");
 
-    GenericPlayer(const string& name):m_Name(name) {};
+    virtual ~GenericPlayer();
 
-    virtual ~GenericPlayer() {};
     // показывает, хочет ли игрок продолжать брать карты
     // Для класса GenericPlayer функция не имеет своей реализации,
     // т.к. для игрока и дилера это будут разные функции
     virtual bool IsHitting() const = 0;
 
-    // возвращает значение, если у игрока перебор -
+    // возвращает значение, если игрок имеет перебор -
     // сумму очков большую 21
     // данная функция не виртуальная, т.к. имеет одинаковую реализацию
     // для игрока и дилера
-    virtual bool IsBusted() const;
+    bool IsBusted() const;
 
     // объявляет, что игрок имеет перебор
     // функция одинакова как для игрока, так и для дилера
     void Bust() const;
 
-    friend ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
-
 protected:
     string m_Name;
 };
 
-bool GenericPlayer::IsBusted() const {
+GenericPlayer::GenericPlayer(const string& name) :
+        m_Name(name)
+{}
+
+GenericPlayer::~GenericPlayer()
+{}
+
+bool GenericPlayer::IsBusted() const
+{
     return (GetTotal() > 21);
 }
 
-void GenericPlayer::Bust() const {
+void GenericPlayer::Bust() const
+{
     cout << m_Name << " busts.\n";
 }
-
-ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
-{
-    os << aGenericPlayer.m_Name << ":\t";
-
-    vector<Card*>::const_iterator pCard;
-    if (!aGenericPlayer.m_Cards.empty())
-    {
-        for (pCard = aGenericPlayer.m_Cards.begin();
-             pCard != aGenericPlayer.m_Cards.end();
-             ++pCard)
-        {
-            os << *(*pCard) << "\t";
-        }
-
-
-        if (aGenericPlayer.GetTotal() != 0)
-        {
-            cout << "(" << aGenericPlayer.GetTotal() << ")";
-        }
-    }
-    else
-    {
-        os << "<empty>";
-    }
-
-    return os;
-}
-
 
 class Player : public GenericPlayer
 {
 public:
-    Player(const string& name = "") {
-        m_Name = name;
-    };
+    Player(const string& name = "");
+
+    virtual ~Player();
 
     // показывает, хочет ли игрок продолжать брать карты
     virtual bool IsHitting() const;
@@ -235,9 +217,14 @@ public:
 
     // объявляет ничью
     void Push() const;
-
-    virtual ~Player() {};
 };
+
+Player::Player(const string& name) :
+        GenericPlayer(name)
+{}
+
+Player::~Player()
+{}
 
 bool Player::IsHitting() const
 {
@@ -246,7 +233,6 @@ bool Player::IsHitting() const
     cin >> response;
     return (response == 'y' || response == 'Y');
 }
-
 
 void Player::Win() const
 {
@@ -263,26 +249,36 @@ void Player::Push() const
     cout << m_Name << " pushes.\n";
 }
 
-class House: public GenericPlayer
+class House : public GenericPlayer
 {
 public:
-    House(const string& name = "House") {};
+    House(const string& name = "House");
+
+    virtual ~House();
 
     // показывает, хочет ли дилер продолжать брать карты
     virtual bool IsHitting() const;
 
     // переворачивает первую карту
     void FlipFirstCard();
-
-    virtual ~House() {};
 };
 
+House::House(const string& name) :
+        GenericPlayer(name)
+{}
 
-bool House::IsHitting() const {
+House::~House()
+{}
+
+// возвращает TRUE, если количество очков меньше или равно 16,
+// и FALSE, если больше 16
+bool House::IsHitting() const
+{
     return (GetTotal() <= 16);
 }
 
-void House::FlipFirstCard() {
+void House::FlipFirstCard()
+{
     if (!(m_Cards.empty()))
     {
         m_Cards[0]->Flip();
@@ -293,15 +289,12 @@ void House::FlipFirstCard() {
     }
 }
 
-
-class Deck : public Hand {
+class Deck : public Hand
+{
 public:
-    Deck() {
-        m_Cards.reserve(52);
-        Populate();
-    }
+    Deck();
 
-    virtual ~Deck() = default;
+    virtual ~Deck();
 
     // создает стандартную колоду из 52 карт
     void Populate();
@@ -316,6 +309,15 @@ public:
     void AdditionalCards(GenericPlayer& aGenericPlayer);
 };
 
+Deck::Deck()
+{
+    m_Cards.reserve(52);
+    Populate();
+}
+
+Deck::~Deck()
+{}
+
 void Deck::Populate()
 {
     Clear();
@@ -329,14 +331,16 @@ void Deck::Populate()
         }
     }
 }
-
+// функция переставляет указатели, расположенные в векторе m_Cards
+// с помощью функции random_shuffle() стандартной библиотеки шаблонов
+// Для использования данной библиотеки необходим файл <algorithm>
 void Deck::Shuffle()
 {
-    random_device rd;
-    mt19937 g(rd());
-    shuffle(m_Cards.begin(), m_Cards.end(), g);
+    random_shuffle(m_Cards.begin(), m_Cards.end());
 }
-
+// функция добавляет в конец вектора m_Cards копию указателя
+// с помощью функции-члена Add().
+// данная функция может работать с объектами классов Player и House
 void Deck::Deal(Hand& aHand)
 {
     if (!m_Cards.empty())
@@ -350,17 +354,14 @@ void Deck::Deal(Hand& aHand)
     }
 }
 
+// функция может работать как с объектами типа Player, так и House
 void Deck::AdditionalCards(GenericPlayer& aGenericPlayer)
 {
     cout << endl;
-    // продолжает раздавать карты до тех пор, пока у игрока не случается
+    //продолжает раздавать карты до тех пор, пока у игрока не случается
     // перебор или пока он хочет взять еще одну карту
-    int i = 0;
     while (!(aGenericPlayer.IsBusted()) && aGenericPlayer.IsHitting())
     {
-        ++i;
-        cout<< aGenericPlayer.GetTotal() << endl;
-        if (i > 10) break;
         Deal(aGenericPlayer);
         cout << aGenericPlayer << endl;
 
@@ -376,7 +377,7 @@ class Game
 public:
     Game(const vector<string>& names);
 
-    ~Game(){};
+    ~Game();
 
     // проводит игру в Blackjack
     void Play();
@@ -388,7 +389,7 @@ private:
 };
 
 // Конструктор этого класса принимает ссылку на вектор строк, представляющих
-// имена игроков-людей. Конструктор создает объект класса Player для каждого имени
+// имена игроков - людей. Конструктор создает объект класса Player для каждого имени
 Game::Game(const vector<string>& names)
 {
     // создает вектор игроков из вектора с именами
@@ -401,10 +402,11 @@ Game::Game(const vector<string>& names)
     // запускает генератор случайных чисел
     srand(static_cast<unsigned int>(time(0)));
     m_Deck.Populate();
-
     m_Deck.Shuffle();
-
 }
+
+Game::~Game()
+{}
 
 void Game::Play()
 {
@@ -412,7 +414,6 @@ void Game::Play()
     vector<Player>::iterator pPlayer;
     for (int i = 0; i < 2; ++i)
     {
-
         for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
         {
             m_Deck.Deal(*pPlayer);
@@ -428,14 +429,13 @@ void Game::Play()
     {
         cout << *pPlayer << endl;
     }
-    cout << "Dealer" << m_House << endl;
+    cout << m_House << endl;
 
     // раздает игрокам дополнительные карты
     for (pPlayer = m_Players.begin(); pPlayer != m_Players.end(); ++pPlayer)
     {
         m_Deck.AdditionalCards(*pPlayer);
     }
-
 
     // показывает первую карту дилера
     m_House.FlipFirstCard();
@@ -488,6 +488,9 @@ void Game::Play()
     m_House.Clear();
 }
 
+// прототипы функций
+ostream& operator<<(ostream& os, const Card& aCard);
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer);
 
 int main()
 {
@@ -521,4 +524,53 @@ int main()
     }
 
     return 0;
+}
+
+// перегружает оператор<<, чтобы получить возможность отправить
+// объект типа Card в поток cout
+ostream& operator<<(ostream& os, const Card& aCard)
+{
+    const string RANKS[] = { "0", "A", "2", "3", "4", "5", "6", "7", "8", "9",
+                             "10", "J", "Q", "K" };
+    const string SUITS[] = { "c", "d", "h", "s" };
+
+    if (aCard.m_IsFaceUp)
+    {
+        os << RANKS[aCard.m_Rank] << SUITS[aCard.m_Suit];
+    }
+    else
+    {
+        os << "XX";
+    }
+
+    return os;
+}
+
+// перегружает оператор<<, чтобы получить возможность отправить
+// объект типа GenericPlayer в поток cout
+ostream& operator<<(ostream& os, const GenericPlayer& aGenericPlayer)
+{
+    os << aGenericPlayer.m_Name << ":\t";
+
+    vector<Card*>::const_iterator pCard;
+    if (!aGenericPlayer.m_Cards.empty())
+    {
+        for (pCard = aGenericPlayer.m_Cards.begin();
+             pCard != aGenericPlayer.m_Cards.end();
+             ++pCard)
+        {
+            os << *(*pCard) << "\t";
+        }
+
+
+        if (aGenericPlayer.GetTotal() != 0)
+        {
+            cout << "(" << aGenericPlayer.GetTotal() << ")";
+        }
+    }
+    else
+    {
+        os << "<empty>";
+    }
+    return os;
 }
